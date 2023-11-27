c6f41c6c345fdb5bb23da4b7631bf494cbaad0d0
thineisan97 <umgodoodev9@gmail.com>
2023-11-27 10:10:27 +0630

    fibonacci series

--- day1/main.py
+++ day1/main.py
@@ -1,4 +1,6 @@
 # standard input/output
+
+# Output
 a=10
 b=15
 print('Hello World {}'.format(a)) 
@@ -8,6 +10,7 @@ print('Hello World %s' % a)
 print('Hello World %f' % a)
 print('Hello World %.2f' % a)
 
+# Input
 # name = 'Thin Ei San'
 name = input('Enter your name : ')
 print(f'Hello {name}')
